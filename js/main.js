b93d7278fab2adae426c08c260c937444e6543ae
midhun manohar <midhmanohar@gmail.com>
2018-02-13 12:28:52 +0530

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-document.getElementById("myForm").addEventListener("submit",)
+
+document.getElementById("myForm").addEventListener("submit",function(e){
+  e.preventDefault();
+let totalClasses=document.getElementById('totalClasses').value;
+let attendedClasses=document.getElementById('attendedClasses').value;
+0
+ // console.log(totalClasses);
+ // console.log(attendedClasses);
+
+let attendance=(attendedClasses/totalClasses)*100;
+
+// console.log(attendance);
+
+while(attendance<75){
+  totalClasses+=1;
+  attendedClasses+=1;
+  attendance=(attendedClasses/totalClasses)*100;
+  console.log(attendance);
+}
+});
