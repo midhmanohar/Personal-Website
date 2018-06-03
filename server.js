eba9730df893d6f338c234a87d840e15193a3f08
midhun manohar <midhmanohar@gmail.com>
2018-06-03 17:40:59 +0530

    bootstrap removed

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/bootstrap.min.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.css'));
-});
-
 app.get('/custom.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'custom.css'));
 });
